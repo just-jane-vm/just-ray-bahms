22d11def19465944b11dc22947f3f2e909a8b54b
jane <yubin.yumiko.ichikawa@gmail.com>
2026-01-24 22:02:34 -0500

    adds support for parsing win strings from config
    AAAA - Red_Epicness

--- just-gamba/src/config_parser.cpp
+++ just-gamba/src/config_parser.cpp
@@ -25,11 +25,19 @@ std::vector<Config> ParseConfig(const char *configPath) {
     std::string label;
     std::string path;
     std::string probability;
+    std::string win2;
+    std::string win3;
     std::getline(ss, label, ',');
     std::getline(ss, path, ',');
     std::getline(ss, probability, ',');
+    std::getline(ss, win2, ',');
+    std::getline(ss, win3, ',');
     float p = std::stof(probability);
-    icons.push_back(Config{.probability = p, .path = path, .label = label});
+    icons.push_back(Config{.probability = p,
+                           .path = path,
+                           .label = label,
+                           .win2 = win2,
+                           .win3 = win3});
   }
 
   return icons;
--- just-gamba/src/config_parser.hpp
+++ just-gamba/src/config_parser.hpp
@@ -8,6 +8,8 @@ struct Config {
   float probability;
   std::string path;
   std::string label;
+  std::string win2;
+  std::string win3;
 };
 
 std::vector<Config> ParseConfig(const char *filePath);
